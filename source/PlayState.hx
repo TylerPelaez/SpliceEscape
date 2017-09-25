596f9c1bfc55536f3aeb41a19693a522e3c6cd0d
Staeliam Schipper-Reyes <staeliam@gmail.com>
2017-09-25 00:22:59 -0400

    Began writing core order list selection functionality

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,8 @@ import flixel.tile.FlxTilemap;
 import openfl.Assets;
 import flixel.FlxSprite;
 import flixel.FlxObject;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
 
 class PlayState extends FlxState
 {
@@ -15,6 +17,15 @@ class PlayState extends FlxState
 	private static var TILE_HEIGHT:Int = 80;
 	private static var TILEMAP_PATH:String = "assets/images/test_tilemap.png";
 	private static var FIRST_LEVEL_NAME:String = "test";
+	// Constants for orders button roll
+	private static var ROLL_X = 0;
+	private static var ROLL_Y = 0;
+	private static var ROLL_SCALE = 3;
+	private static var ROLL_COUNT = 4;
+	private static var ROLL_SPACING = 200;
+	private static var ROLL_TEXT_PIXELS = 32;
+	private static var ROLL_SELECT_DROP = 200;
+	private static var SELECT_PIXELS = 32;	
 	// Instructions to be initialized in create()
 	// After player chooses instructions - copies will be made and added to 
 	// the player instruction list.
@@ -34,7 +45,17 @@ class PlayState extends FlxState
 
 	private var _selectedInstructionList:List<Instruction>;
 
+	private var _availableInstructionList:Array<List<Instruction> >;
+	private var _subInstructionList:List<List<Instruction> >;
+
 	private var _mouseWrapper:FlxSprite;
+
+	private var _orderDisplay:FlxText;
+	private var _orders:Array<FlxButton>;
+	private var _rollLeft:FlxButton;
+	private var _rollRight:FlxButton;
+	private var _orderBase:Int;
+
 
 	override public function create():Void
 	{
@@ -45,8 +66,43 @@ class PlayState extends FlxState
 		_levels = new Array<LevelData>();
 		_selectedInstructionList = new List<Instruction>();
 		initInstructions();
+		_availableInstructionList = new Array<List< Instruction> >();
+		_subInstructionList = new List<List< Instruction> >();
 		_mouseWrapper = new FlxSprite();
 		_inViewMode = false;
+
+		_orderDisplay = new FlxText();
+		_orderDisplay.x = 400;
+		_orderDisplay.y = ROLL_X + ROLL_SELECT_DROP;
+		_orderDisplay.setFormat(null,SELECT_PIXELS);
+		//_orderDisplay.exists = false;
+
+		_orders = new Array<FlxButton>();
+
+		_orderBase = 0;
+		
+		_rollRight = new FlxButton(0,0,"→",function(){_orderBase++;});
+		_rollRight.scale.y = ROLL_SCALE;
+		_rollRight.label.setFormat(null,ROLL_PIXELS);
+		//_rollRight.exists = false;
+
+		_rollLeft = new FlxButton(0,0,"←",function(){_orderBase--;});
+		_rollLeft.scale.y = ROLL_SCALE;
+		_rollLeft.label.setFormat(null,ROLL_PIXELS);
+		_rollLeft.x = (ROLL_COUNT + 2)*ROLL_SPACING;
+		//_rollRight.exists = false;
+
+		//Generate ROLL_COUNT buttons, set them to be scaled and formatted appropriately.
+		for(i in 0...ROLL_COUNT)
+		{
+			_orders.insert(0,new FlxButton());
+			_orders[0].x = (ROLL_COUNT + 1)*ROLL_SPACING - i;
+			_orders[0].scale.x = _orders[0].scale.y = ROLL_SCALE;
+			_orders[0].label.setFormat(null,ROLL_PIXELS);
+			//_orders[0].exists = false;
+			add(_orders[0]);
+		}
+
 
 		add(_collisionMap);
 		add(_player);
@@ -95,6 +151,36 @@ class PlayState extends FlxState
 			resetPlayerPlayMode();
 		}
 		super.update(elapsed);
+	}
+
+	private function setOrdersState()
+	{
+		for(i in 0...ROLL_COUNT)
+		{
+			_orders[i].label.text = "";
+			_orders[i].onUp.callback = function(){};
+			//If in range, bind the button to adding the order list and add its text to the button.
+			if(i+_orderBase < _availableInstructionList.length && i+_orderBase >= 0)
+			{
+				for(ins in _availableInstructionList[i])
+				{
+					_orders[i].label.text += ins._name;
+				}
+				_orders[i].onUp.callback = function(){
+					_subInstructionList.add(_availableInstructionList[i]);
+					//Set the orders buttons again
+					setOrdersState();
+				}
+			}
+		}
+		_orderDisplay.text = "";
+		for(sublist in _subInstructionList)
+		{
+			for(ins in sublist)
+			{
+				_orderDisplay.text += ins._name;
+			}
+		}
 	}
 
 	private function loadlevelsFromFile(firstLevelName:String):Void{
@@ -146,12 +232,12 @@ class PlayState extends FlxState
 	private function initInstructions():Void
 	{
 		// Instructions that can be copied and then given to the player's instruction list.
-		WALK_LEFT_INSTRUCTION = new Instruction("Walk Left", 2, -200, 0, true);
-		WALK_RIGHT_INSTRUCTION = new Instruction("Walk Right", 2, 200, 0, false);
-		JUMP_RIGHT_INSTRUCTION = new Instruction("Jump Right", 1.25, 200, -1000, false);
-		JUMP_LEFT_INSTRUCTION = new Instruction("Jump Left", 1.25, -200, -1000, true);
-		IDLE_INSTRUCTION = new Instruction("Idle", 2, 0, 0, false);
-		INTERACT_INSTRUCTION = new Instruction("Interact", 0.5, 0, 0, false, true);
+		WALK_LEFT_INSTRUCTION = new Instruction("←", 2, -200, 0, true);
+		WALK_RIGHT_INSTRUCTION = new Instruction("→", 2, 200, 0, false);
+		JUMP_RIGHT_INSTRUCTION = new Instruction("↗", 1.25, 200, -1000, false);
+		JUMP_LEFT_INSTRUCTION = new Instruction("↖", 1.25, -200, -1000, true);
+		IDLE_INSTRUCTION = new Instruction("0", 2, 0, 0, false);
+		INTERACT_INSTRUCTION = new Instruction("I", 0.5, 0, 0, false, true);
 	}
 
 	private function resetPlayerViewMode()
