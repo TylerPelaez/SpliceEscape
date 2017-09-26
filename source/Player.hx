536cedc496e7c8b444549550bead53e23a781dc0
Staeliam Schipper-Reyes <staeliam@gmail.com>
2017-09-26 17:23:47 -0400

    Merged in order queueing system & sound proto

--- source/Player.hx
+++ source/Player.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.math.FlxPoint;
 import flixel.FlxObject;
+import flixel.system.FlxSound;
 
 // enum Instructions {
 //     Idle;
@@ -20,6 +21,8 @@ class Player extends FlxSprite {
     private var _speed:Float;
     private var _isActive:Bool;
 
+    private var _sndEngine:FlxSound;
+
     public function new() {
         super();
         loadGraphic("assets/images/duck.png", true, 100, 114);
@@ -30,6 +33,8 @@ class Player extends FlxSprite {
 		setFacingFlip(FlxObject.RIGHT, false, false);
 
         acceleration.y = 750; // Gravity is positive because Y increases downwards.
+
+        _sndEngine = FlxG.sound.load(AssetPaths.RobotEngine__wav);
     }
 
     override public function update(elapsed:Float):Void {
@@ -38,7 +43,11 @@ class Player extends FlxSprite {
         {
             updateInstruction(elapsed);
 		    movement();
+
+            _sndEngine.play();
         }
+        else
+            _sndEngine.pause();
         super.update(elapsed);
     }
 
@@ -48,10 +57,10 @@ class Player extends FlxSprite {
     **/
     private function updateInstruction(elapsed:Float):Void {
         _instructionTimer -= elapsed;
-        if ( _instructionTimer > 0.0 )
+        //Checking if absolute y vel > 20 ensures that if we're airborne we can't get new instructions(zero air maneuverabiltiy)
+        if ( _instructionTimer > 0.0 || (velocity.y > 20 || velocity.y < -20))
             return;
 
-        // Temporary until Spliced Order Queuing is done.
         if (!_instructionList.isEmpty())
         {
             _currentInstruction = _instructionList.pop();
