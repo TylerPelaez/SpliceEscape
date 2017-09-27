492dea5c4ce9fb00eecce24d6ae1d0402eaa1310
Pheonix <colecarpenter@gmail.com>
2017-09-27 11:27:41 -0400

    updated files for level 4, added level 5 tilemap

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -17,7 +17,7 @@ class PlayState extends FlxState
 	private static var TILE_WIDTH:Int = 128;
 	private static var TILE_HEIGHT:Int = 128;
 	private static var TILEMAP_PATH:String = "assets/images/tilemap_v1.png";
-	private static var FIRST_LEVEL_NAME:String = "lvl_3";
+	private static var FIRST_LEVEL_NAME:String = "lvl_5";
 	// Constants for orders button roll
 	private static var ROLL_X:Int = 150;
 	private static var ROLL_Y:Int = 150;
