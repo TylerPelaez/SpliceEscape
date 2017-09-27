812f3b84fd3772b9d27defd5ae1a398b8409f542
Axel <stewaa5@rpi.edu>
2017-09-26 22:36:33 -0400

    Added pickup sound and created an extremely hacky commented-out framework for detecting fall.

--- source/Box.hx
+++ source/Box.hx
@@ -1,12 +1,19 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.system.FlxSound;
 
 class Box extends FlxSprite
 {
     public var _beingHeld:Bool;
     private var _initPosX:Int;
     private var _initPosY:Int;
+    private var _sndPickup:FlxSound;
+    private var _sndDrop:FlxSound;
+
+    // private var _prevY:Float;
+    // private var _prevPrevY:Float;
 
     public function new(posX:Int, posY:Int)
     {
@@ -14,7 +21,19 @@ class Box extends FlxSprite
         _initPosX = posX;
         _initPosY = posY;
         resetToInitPos();
+        _sndPickup = FlxG.sound.load(AssetPaths.boxtake__wav);
+        _sndDrop = FlxG.sound.load(AssetPaths.boxdrop__wav);
     }
+    
+	// override public function update(elapsed:Float):Void
+    // {
+        // if (_prevY == y && _prevPrevY < _prevY + .1)
+        // {
+            // _sndDrop.play();
+        // }
+        // _prevPrevY = _prevY;
+        // _prevY = y;
+    // }
 
     public function resetToInitPos():Void
     {
@@ -26,6 +45,7 @@ class Box extends FlxSprite
     {
         _beingHeld = true;
         acceleration.y = 0;
+        _sndPickup.play();
     }
 
     public function drop()
